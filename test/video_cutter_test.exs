245f8f6aa468690fb733d79b59332ecbf3bf8fec
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-06-30 11:05:24 +0200

    add testing offset

--- test/video_cutter_test.exs
+++ test/video_cutter_test.exs
@@ -9,7 +9,7 @@ defmodule Membrane.VideoMerger.VideoCutterTest do
 
   @framerate 30
 
-  def make_pipeline_with_testing_sink(intervals) do
+  def make_pipeline_with_testing_sink(video_cutter) do
     Testing.Pipeline.start_link(%Testing.Pipeline.Options{
       elements: [
         file_src: %Membrane.File.Source{
@@ -19,17 +19,19 @@ defmodule Membrane.VideoMerger.VideoCutterTest do
         },
         parser: %H264.FFmpeg.Parser{framerate: {@framerate, 1}},
         decoder: %H264.FFmpeg.Decoder{add_pts?: true},
-        video_cutter: %VideoCutter{
-          intervals: intervals
-        },
+        video_cutter: video_cutter,
         sink: Testing.Sink
       ]
     })
   end
 
-  test "cut middle 3 seconds" do
+  test "cut middle 3s" do
     frame_duration = Ratio.new(Time.second(), @framerate)
     intervals = [{0, Time.second()}, {4 * Time.second(), :infinity}]
+
+    video_cutter = %VideoCutter{
+      intervals: intervals
+    }
 
     # input video has 10[s] * 30[frames/s] = 300[frames]
     # there should be 300[frames] - (4-1)[s] * 30[frames/s] = 210[frames] on the testing sink with the following timestamps
@@ -37,7 +39,39 @@ defmodule Membrane.VideoMerger.VideoCutterTest do
     second_interval_pts_list = for i <- 120..299, do: Ratio.mult(frame_duration, i)
     expected_pts_list = first_interval_pts_list ++ second_interval_pts_list
 
-    assert {:ok, pid} = make_pipeline_with_testing_sink(intervals)
+    assert {:ok, pid} = make_pipeline_with_testing_sink(video_cutter)
+    assert Testing.Pipeline.play(pid) == :ok
+
+    expected_pts_list
+    |> Enum.each(fn expected_pts ->
+      assert_sink_buffer(pid, :sink, %Membrane.Buffer{metadata: %{pts: buffer_pts}})
+      assert expected_pts == buffer_pts
+    end)
+
+    assert_end_of_stream(pid, :sink, :input, 10_000)
+
+    Testing.Pipeline.stop_and_terminate(pid, blocking?: true)
+  end
+
+  test "cut middle 3s and apply 0.5s offset" do
+    frame_duration = Ratio.new(Time.second(), @framerate)
+    intervals = [{0, Time.second()}, {4 * Time.second(), :infinity}]
+    offset = Ratio.new(Time.second(), 2)
+
+    video_cutter = %VideoCutter{
+      intervals: intervals,
+      offset: offset
+    }
+
+    first_interval_pts_list =
+      for i <- 0..29, do: Ratio.mult(frame_duration, i) |> Ratio.add(offset)
+
+    second_interval_pts_list =
+      for i <- 120..299, do: Ratio.mult(frame_duration, i) |> Ratio.add(offset)
+
+    expected_pts_list = first_interval_pts_list ++ second_interval_pts_list
+
+    assert {:ok, pid} = make_pipeline_with_testing_sink(video_cutter)
     assert Testing.Pipeline.play(pid) == :ok
 
     expected_pts_list
