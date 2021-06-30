307b690de35fa5bab44ef64338fbe1fcda5784bf
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-06-30 10:59:51 +0200

    first sketch of videocutter tested

--- lib/video_cutter/video_cutter.ex
+++ lib/video_cutter/video_cutter.ex
@@ -1,8 +1,24 @@
 defmodule Membrane.VideoCutter do
   use Membrane.Filter
 
-  alias Membrane.Buffer
   alias Membrane.Caps.Video.Raw
+
+  def_options intervals: [
+                spec: [{pos_integer(), pos_integer() | :infinity}],
+                default: [{0, :infinity}],
+                description: """
+                List of intervals of timestamps. The buffer is forwarder when its timestamp belongs to any of the given intervals. The start of the interval is inclusive and the end is exclusive.
+                By default, the cutter is initialized with a single interval [0, :infinity)
+                """
+              ],
+              offset: [
+                spec: pos_integer(),
+                default: 0,
+                description: """
+                Offset is applied to all output frames pts values. It allows to logically shift the video to express its real starting point. For example, if there are two streams and
+                the second one begins two seconds after the first one, video cutter that processes the second stream should apply a 2sec offset. Offset is applied after cutting phase.
+                """
+              ]
 
   def_output_pad :output,
     caps: {Raw, format: one_of([:I420, :I422]), aligned: true}
@@ -12,9 +28,8 @@ defmodule Membrane.VideoCutter do
     demand_unit: :buffers
 
   @impl true
-  def handle_init(_intervals) do
-    # {:ok, %{intervals: intervals}}
-    {:ok, %{caps_sent: false}}
+  def handle_init(opts) do
+    {:ok, opts}
   end
 
   @impl true
@@ -23,28 +38,20 @@ defmodule Membrane.VideoCutter do
   end
 
   @impl true
-  def handle_caps(:input, caps, _ctx, state) do
-    {:ok, state}
-  end
-
-  @impl true
   def handle_prepared_to_playing(_ctx, state) do
     {:ok, state}
   end
 
-  def handle_process(:input, buffer, ctx, state) do
-    IO.inspect("[Video cutter] Processing...")
-    buffer_action = [buffer: {:output, [buffer]}]
+  def handle_process(:input, buffer, _ctx, state) do
+    if not Map.has_key?(buffer.metadata, :pts), do: raise("Cannot cut stream without pts")
 
-    {caps_action, state} =
-      if ctx.pads.input.caps != nil and state.caps_sent == false do
-        {[caps: {:output, ctx.pads.input.caps}], %{state | caps_sent: true}}
-      else
-        {[], state}
-      end
+    buffer_action =
+      if check_buffer(buffer.metadata.pts, state.intervals),
+        do: [buffer: {:output, [apply_offset(buffer, state.offset)]}],
+        else: []
 
-    actions = buffer_action ++ caps_action ++ [redemand: :output]
-    IO.inspect(actions, label: "$$$")
+    actions = buffer_action ++ [redemand: :output]
+
     {{:ok, actions}, state}
   end
 
@@ -53,9 +60,22 @@ defmodule Membrane.VideoCutter do
     {{:ok, [end_of_stream: :output, notify: {:end_of_stream, :input}]}, state}
   end
 
-  defp check_buffer(timestamp, ranges) do
-    Enum.any?(ranges, fn %{start: start, stop: stop} ->
-      timestamp >= start and timestamp < stop
+  defp apply_offset(buffer, offset) do
+    Bunch.Struct.update_in(buffer, [:metadata, :pts], &Ratio.add(&1, offset))
+  end
+
+  defp check_buffer(timestamp, intervals) do
+    use Ratio
+
+    Enum.any?(intervals, fn
+      {:infinity, _any} ->
+        false
+
+      {start, :infinity} ->
+        Ratio.gte?(timestamp, start)
+
+      {start, stop} ->
+        Ratio.gte?(timestamp, start) and Ratio.lt?(timestamp, stop)
     end)
   end
 end
--- test/video_cutter_test.exs
+++ test/video_cutter_test.exs
@@ -4,31 +4,50 @@ defmodule Membrane.VideoMerger.VideoCutterTest do
   import Membrane.Testing.Assertions
   alias Membrane.VideoCutter
   alias Membrane.H264
-  alias Membrane.Testing.Pipeline
+  alias Membrane.Testing
+  alias Membrane.Time
 
-  def make_pipeline() do
-    Pipeline.start_link(%Pipeline.Options{
+  @framerate 30
+
+  def make_pipeline_with_testing_sink(intervals) do
+    Testing.Pipeline.start_link(%Testing.Pipeline.Options{
       elements: [
         file_src: %Membrane.File.Source{
           chunk_size: 40_960,
           location:
             "/Users/andrzej/Membrane/membrane_video_merger_plugin/test/fixtures/test_video_10s.h264"
         },
-        parser: H264.FFmpeg.Parser,
-        decoder: H264.FFmpeg.Decoder,
-        video_cutter: VideoCutter,
-        encoder: %H264.FFmpeg.Encoder{preset: :fast, crf: 30},
-        sink: %Membrane.File.Sink{
-          location:
-            "/Users/andrzej/Membrane/membrane_video_merger_plugin/test/results/test_video_10s.h264"
-        }
+        parser: %H264.FFmpeg.Parser{framerate: {@framerate, 1}},
+        decoder: %H264.FFmpeg.Decoder{add_pts?: true},
+        video_cutter: %VideoCutter{
+          intervals: intervals
+        },
+        sink: Testing.Sink
       ]
     })
   end
 
-  test "simple test" do
-    assert {:ok, pid} = make_pipeline()
-    assert Pipeline.play(pid) == :ok
+  test "cut middle 3 seconds" do
+    frame_duration = Ratio.new(Time.second(), @framerate)
+    intervals = [{0, Time.second()}, {4 * Time.second(), :infinity}]
+
+    # input video has 10[s] * 30[frames/s] = 300[frames]
+    # there should be 300[frames] - (4-1)[s] * 30[frames/s] = 210[frames] on the testing sink with the following timestamps
+    first_interval_pts_list = for i <- 0..29, do: Ratio.mult(frame_duration, i)
+    second_interval_pts_list = for i <- 120..299, do: Ratio.mult(frame_duration, i)
+    expected_pts_list = first_interval_pts_list ++ second_interval_pts_list
+
+    assert {:ok, pid} = make_pipeline_with_testing_sink(intervals)
+    assert Testing.Pipeline.play(pid) == :ok
+
+    expected_pts_list
+    |> Enum.each(fn expected_pts ->
+      assert_sink_buffer(pid, :sink, %Membrane.Buffer{metadata: %{pts: buffer_pts}})
+      assert expected_pts == buffer_pts
+    end)
+
     assert_end_of_stream(pid, :sink, :input, 10_000)
+
+    Testing.Pipeline.stop_and_terminate(pid, blocking?: true)
   end
 end
