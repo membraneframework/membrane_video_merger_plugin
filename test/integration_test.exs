443670f49ada167fb2f2617c7eb38b46e3a6f26b
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2021-07-16 15:59:04 +0200

    Add more integration tests
    Move common part of tests to separate function

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -9,59 +9,95 @@ defmodule Membrane.VideoMerger.IntegrationTest do
   @fps 30
   @framerate {@fps, 1}
 
-  test "split into two parts and merge again" do
-    elems = [
-      file_src_1: %File.Source{
-        chunk_size: 40_960,
-        location: "./test/fixtures/test_video_10s.h264"
-      },
-      parser_1: %H264.FFmpeg.Parser{framerate: @framerate},
-      decoder_1: H264.FFmpeg.Decoder,
-      file_src_2: %File.Source{
-        chunk_size: 40_960,
-        location: "./test/fixtures/test_video_10s.h264"
-      },
-      parser_2: %H264.FFmpeg.Parser{framerate: @framerate},
-      decoder_2: H264.FFmpeg.Decoder,
-      cutter_1: %VideoCutter{intervals: [{0, Time.seconds(1)}]},
-      cutter_2: %VideoCutter{intervals: [{Time.seconds(1), Time.seconds(2)}]},
-      merger: VideoMerger,
-      sink: Testing.Sink
-    ]
-
-    links = [
-      link(:file_src_1)
-      |> to(:parser_1)
-      |> to(:decoder_1)
-      |> to(:cutter_1)
-      |> via_in(Pad.ref(:input, 1))
-      |> to(:merger)
-      |> to(:sink),
-      link(:file_src_2)
-      |> to(:parser_2)
-      |> to(:decoder_2)
-      |> to(:cutter_2)
-      |> via_in(Pad.ref(:input, 2))
-      |> to(:merger)
-    ]
-
-    assert {:ok, pid} =
-             Testing.Pipeline.start_link(%Testing.Pipeline.Options{
-               elements: elems,
-               links: links
-             })
-
-    assert Testing.Pipeline.play(pid) == :ok
+  defp run_multiple_cutter_test(cutters, test_length) do
+    assert {:ok, pid} = start_multiple_cutter_pipeline(cutters)
+    assert(Testing.Pipeline.play(pid) == :ok)
     frame_duration = Ratio.new(Time.second(), @fps)
 
     assert_end_of_stream(pid, :sink, :input, 10_000)
 
-    for i <- 0..59 do
+    for i <- 0..(test_length * @fps - 1) do
       pts = Ratio.mult(frame_duration, i)
       assert_sink_buffer(pid, :sink, %Membrane.Buffer{metadata: %{pts: buffer_pts}})
       assert pts == buffer_pts
     end
 
     Testing.Pipeline.stop_and_terminate(pid, blocking?: true)
+  end
+
+  defp start_multiple_cutter_pipeline(cutters) do
+    elems = [merger: VideoMerger, sink: Testing.Sink]
+    links = [link(:merger) |> to(:sink)]
+
+    {_i, elems, links} =
+      List.foldl(cutters, {1, elems, links}, fn cutter, {i, elems, links} ->
+        {src_i, parser_i, decoder_i, cutter_i} =
+          {String.to_atom("file_src_#{i}"), String.to_atom("parser_#{i}"),
+           String.to_atom("decoder_#{i}"), String.to_atom("cutter_#{i}")}
+
+        new_elems = [
+          {src_i,
+           %File.Source{
+             chunk_size: 40_960,
+             location: "./test/fixtures/test_video_10s.h264"
+           }},
+          {parser_i, %H264.FFmpeg.Parser{framerate: @framerate}},
+          {decoder_i, H264.FFmpeg.Decoder},
+          {cutter_i, cutter}
+        ]
+
+        new_links = [
+          link(src_i)
+          |> to(parser_i)
+          |> to(decoder_i)
+          |> to(cutter_i)
+          |> via_in(Pad.ref(:input, i))
+          |> to(:merger)
+        ]
+
+        {i + 1, elems ++ new_elems, links ++ new_links}
+      end)
+
+    Testing.Pipeline.start_link(%Testing.Pipeline.Options{
+      elements: elems,
+      links: links
+    })
+  end
+
+  test "split into two parts and merge again" do
+    cutters = [
+      %VideoCutter{intervals: [{0, Time.seconds(2)}]},
+      %VideoCutter{intervals: [{Time.seconds(2), :infinity}]}
+    ]
+
+    run_multiple_cutter_test(cutters, 10)
+  end
+
+  test "split into four parts and merge again" do
+    cutters = [
+      %VideoCutter{intervals: [{0, Time.seconds(2)}, {Time.seconds(8), :infinity}]},
+      %VideoCutter{intervals: [{Time.seconds(2), Time.seconds(4)}]},
+      %VideoCutter{intervals: [{Time.seconds(4), Time.seconds(6)}]},
+      %VideoCutter{intervals: [{Time.seconds(6), Time.seconds(8)}]}
+    ]
+
+    run_multiple_cutter_test(cutters, 10)
+  end
+
+  test "merge only first two seconds of audio" do
+    cutters = [
+      %VideoCutter{intervals: [{0, Time.seconds(1)}]},
+      %VideoCutter{intervals: [{Time.seconds(1), Time.seconds(2)}]}
+    ]
+
+    run_multiple_cutter_test(cutters, 2)
+  end
+
+  test "single cutter" do
+    cutters = [
+      %VideoCutter{intervals: [{Time.seconds(0), :infinity}]}
+    ]
+
+    run_multiple_cutter_test(cutters, 10)
   end
 end
