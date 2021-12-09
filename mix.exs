923bc052103fc09e071afe6aedd55c3a2f45979e
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2021-12-09 22:10:07 +0100

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.VideoMerger.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @github_url "https://github.com/membraneframework/membrane_video_merger_plugin"
 
   def project do
