4b9fc134b7772c5b5a1a017811c9ce4d9072d9e0
Agnieszka Dutka <agnieszka.dutka@swmansion.com>
2021-12-13 12:33:49 +0100

    release v0.1.2 - merger redemand patch

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.VideoMerger.Mixfile do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @github_url "https://github.com/membraneframework/membrane_video_merger_plugin"
 
   def project do
