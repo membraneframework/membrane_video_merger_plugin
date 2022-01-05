a3587ebb929dd1ae92a9868ef397e5d7efc245c1
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2022-01-05 17:04:12 +0100

    Make license SPDX compatible

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule Membrane.VideoMerger.Mixfile do
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
