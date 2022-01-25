944d1696d6fc3045545208e8844d37d0ecc75675
Agnieszka Dutka <agnieszka.dutka@swmansion.com>
2022-01-25 12:11:31 +0100

    release v0.3.1 - add missing redemand in VideoMerger

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.VideoMerger.Mixfile do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @github_url "https://github.com/membraneframework/membrane_video_merger_plugin"
 
   def project do
